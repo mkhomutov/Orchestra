f7805293d9b7d4b693e53e19920bc3ddaa9a0ea6
Maxim <mkhomutov@gmail.com>
2013-08-07 22:38:04 +0400

    Initial commit for Julia syntax Highlighting.

---
+++ src/Orchestra.Modules.Julia/JuliaModule.cs
@@ -0,0 +1,59 @@
+﻿namespace Orchestra.Modules.Julia
+{
+    using Catel.MVVM;
+    using Catel.MVVM.Services;
+    using Orchestra.Models;
+    using Orchestra.Modules;
+    using Orchestra.Modules.TextEditor.Services;
+    using Orchestra.Modules.TextEditor.Services.Interfaces;
+    using Orchestra.Services;
+
+    /// <summary>
+    /// The module.
+    /// </summary>
+    public class JuliaModule : Orchestra.Modules.ModuleBase
+    {
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        public JuliaModule()
+            : base("JuliaModule")
+        {
+        }
+
+        /// <summary>
+        /// Called when the module is initialized.
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            var orchestraService = GetService<IOrchestraService>();
+
+            // TODO: Register ribbon items
+            //var openRibbonItem = new RibbonItem(ModuleName, ModuleName, "Action name", new Command(() =>
+            //    {
+            //        // Action to invoke
+            //    }));
+            //orchestraService.AddRibbonItem(openRibbonItem);
+
+            // TODO: Handle additional initialization code
+        }
+
+        /// <summary>
+        /// Initializes the ribbon.
+        /// <para />
+        /// Use this method to hook up views to ribbon items.
+        /// </summary>
+        /// <param name="ribbonService">The ribbon service.</param>
+        protected override void InitializeRibbon(IRibbonService ribbonService)
+        {
+            ribbonService.RegisterRibbonItem(new RibbonButton(HomeRibbonTabName, ModuleName, "Julia", new Command(OnShowJuliaEditor)));
+        }
+
+        private static void OnShowJuliaEditor()
+        {
+            var textEditor = (ITextEditorManager)Catel.IoC.ServiceLocator.Default.GetService(typeof(ITextEditorManager));
+            textEditor.AddHighlightingSchema("Julia", new [] { "*.jl" }, Properties.Resources.JuliaLang);
+            textEditor.ShowEditor();
+        }
+    }
+}
---
+++ src/Orchestra.Modules.TextEditor/Views/TextEditorView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orchestra.Modules.JuliaEditor.Views
+{
+    using Orchestra.Views;
+
+    /// <summary>
+    /// Interaction logic for JuliaLangEditorView.xaml.
+    /// </summary>
+    public partial class TextEditorView : DocumentView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextEditorView"/> class.
+        /// </summary>
+        public TextEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
