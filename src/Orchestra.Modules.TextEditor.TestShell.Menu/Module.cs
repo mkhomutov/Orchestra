066e2d3fb08356614585a93445a3c1515ce0cfe7
Maxim <mkhomutov@gmail.com>
2013-09-27 16:20:24 +0400

    Added shell for testing TextEditor

---
+++ src/Orchestra.Modules.TextEditor.TestShell.Menu/Module.cs
@@ -0,0 +1,55 @@
+﻿namespace Orchestra.Modules.TextEditor.TestShell.Menu
+{
+    using Catel.IoC;
+    using Catel.MVVM;
+
+    using Orchestra.Models;
+    using Orchestra.Modules.TextEditor.Services.Interfaces;
+    using Orchestra.Services;
+
+    /// <summary>
+    /// The module.
+    /// </summary>
+    public class MyModule : ModuleBase
+    {
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        public MyModule()
+            : base("MyModule")
+        {
+        }
+
+        /// <summary>
+        /// Called when the module is initialized.
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            ConfigureTextEditor();
+        }
+
+        protected override void InitializeRibbon(IRibbonService ribbonService)
+        {
+            ribbonService.RegisterRibbonItem(new RibbonButton(HomeRibbonTabName, ModuleName, "TextEditor", new Command(OnShowTextEditor)));
+
+            base.InitializeRibbon(ribbonService);
+        }
+
+        private static void OnShowTextEditor()
+        {
+            var textEditorService = (ITextEditorService)ServiceLocator.Default.ResolveType(typeof(ITextEditorService));
+            var documentService = (IDocumentService)ServiceLocator.Default.ResolveType(typeof(IDocumentService));
+
+            var document = textEditorService.CreateDocument("TestConfiguration");            
+            documentService.SetHighlighting(document, "*.txt");
+        }
+
+        private static void ConfigureTextEditor()
+        {
+            var textEditorService = DependencyResolverManager.Default.DefaultDependencyResolver.Resolve<ITextEditorService>();
+
+            textEditorService.Configure("TestConfiguration")
+                             .Apply();
+        }
+    }
+}
---
+++ src/Orchestra.Modules.TextEditor.TestShell/App.xaml.cs
@@ -0,0 +1,79 @@
+﻿namespace Orchestra.Modules.TextEditor.TestShell
+{
+    using System;
+    using System.Windows;
+
+    using Catel;
+    using Catel.IoC;
+    using Catel.MVVM;
+    using Catel.MVVM.ViewModels;
+    using Catel.Windows;
+
+    using Orchestra;
+    using Orchestra.Services;
+    using Orchestra.ViewModels;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Application.Startup"/> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.Windows.StartupEventArgs"/> that contains the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+/*
+            StyleHelper.CreateStyleForwardersForDefaultStyles(Current.Resources.MergedDictionaries[1]);
+
+            var serviceLocator = ServiceLocator.Instance;
+            Environment.RegisterDefaultViewModelServices();
+
+            var viewLocator = serviceLocator.ResolveType<IViewLocator>();
+            viewLocator.Register(typeof(ProgressNotifyableViewModel), typeof(Orchestra.Views.SplashScreen));
+
+            var viewModelLocator = serviceLocator.ResolveType<IViewModelLocator>();
+            viewModelLocator.Register(typeof(Orchestra.Views.SplashScreen), typeof(ProgressNotifyableViewModel));
+
+            var bootstrapper = new OrchestraBootstrapper();
+            bootstrapper.RunWithSplashScreen<ProgressNotifyableViewModel>();*/
+
+            // Example of best performance options for Catel (but at the cost of validation features)
+            Catel.Windows.Controls.UserControl.DefaultCreateWarningAndErrorValidatorForViewModelValue = false;
+            Catel.Windows.Controls.UserControl.DefaultSkipSearchingForInfoBarMessageControlValue = true;
+            Catel.Data.ModelBase.SuspendValidationForAllModels = true;
+
+            var serviceLocator = ServiceLocator.Default;
+            Catel.Environment.RegisterDefaultViewModelServices();
+
+            var viewLocator = serviceLocator.ResolveType<IViewLocator>();
+            viewLocator.Register(typeof(SplashScreenViewModel), typeof(Views.SplashScreen));
+
+            var viewModelLocator = serviceLocator.ResolveType<IViewModelLocator>();
+            viewModelLocator.Register(typeof(Views.SplashScreen), typeof(SplashScreenViewModel));
+
+            var bootstrapper = new OrchestraBootstrapper();
+
+            StyleHelper.CreateStyleForwardersForDefaultStyles(Current.Resources.MergedDictionaries[1]);
+
+/*            bootstrapper.CreatedShell += (sender, e2) =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    // Configure shell when it's created
+                    var configurationService = ServiceLocator.Default.ResolveType<IConfigurationService>();
+
+                    // Disable debugging window
+                    var orchestraService = ServiceLocator.Default.ResolveType<IOrchestraService>();
+                    orchestraService.ShowDebuggingWindow = false;
+                });
+            };*/
+
+            bootstrapper.RunWithSplashScreen<SplashScreenViewModel>();
+
+            base.OnStartup(e);
+        }
+    }
+}
